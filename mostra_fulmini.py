76da12b40ebcf058d69e2957c50f8ff83b35d0e1
root <root@TESTV-DCKRSWRM2.arpa.local>
2019-01-03 13:21:21 +0000

    flask debug

--- mostra_fulmini.py
+++ mostra_fulmini.py
@@ -6,15 +6,15 @@ Created on Tue Oct  2 10:07:26 2018
 """
 from flask import Flask
 from flask import render_template
-from flask_s3 import FlaskS3
+#from flask_s3 import FlaskS3
 app = Flask(__name__)
 app.config['FLASKS3_BUCKET_NAME']='lampinet'
 app.config['AWS_ACCESS_KEY_ID']='ACCESS_KEY'
 app.config['AWS_SECRET_ACCESS_KEY']='SECRET_KEY'
 
-s3=FlaskS3(app)
+#s3=FlaskS3(app)
 @app.route("/")
 def hello():
     return render_template('skeleton.html')
 if __name__=="__main__":
-    app.run(port=8890,debug=True)
+    app.run(host='0.0.0.0',port=8890,debug=True)
