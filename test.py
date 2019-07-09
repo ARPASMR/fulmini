5a63127122ee71a24dfa823a4861ac7bcbf66b56
mauromussin <m.mussin@arpalombardia.it>
2019-07-09 09:34:38 +0200

    Delete test.py

--- test.py
+++
@@ -1,7 +0,0 @@
-nomefile='20190118.dat'
-import pandas as pd
-df=pd.read_csv(filepath_or_buffer=nomefile,sep='\s+', names=['date','time','lat','lon','int','unit','ground'],parse_dates={'datetime':['date','time']})
-#import gf
-#gf.graf(nomefile,df)
-import gf2
-gf2.graf(nomefile)
